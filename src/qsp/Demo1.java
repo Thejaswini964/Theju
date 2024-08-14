65b007d5150b942e66270d0d7aaddeb16004a9e2
theja <theja@LAPTOP-IVSGH2HS>
2024-08-14 16:36:14 +0530

    Commit 2

---
+++ src/qsp/Demo1.java
@@ -0,0 +1,21 @@
+package qsp;
+
+public class Demo1 {
+
+
+		 public static void main(String[] args) {
+				String name="Theju";
+				String rev=" ";
+				for(int i=name.length()-1;i>=0;i--)
+				{
+					rev=rev+name.charAt(i);
+				}
+		         System.out.println(rev);
+			}
+
+		}
+
+
+	
+
+
